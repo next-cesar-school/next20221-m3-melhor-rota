6cc85ee4b9c5fa918632e1af0c0396c282891c73
Diego Lins <diegotec.lins@gmail.com>
2022-06-10 13:44:08 -0300

    criacao da entidade Truck e inserido algoritmo

---
+++ src/main/java/com/nextm3project/bestRoute/BestRoute.java
@@ -0,0 +1,106 @@
+package com.nextm3project.bestRoute;
+
+import java.util.Scanner;
+
+public class BestRoute 
+{
+    public static int[][] shortestpath(int[][] adj, int[][] path) 
+    {
+        int n = adj.length;
+        int[][] ans = new int[n][n];
+        copy(ans, adj);
+    	for (int k=0; k<n;k++) 
+            for (int i=0; i<n; i++) 
+        	for (int j=0; j<n;j++) 
+                    if (ans[i][k]+ans[k][j] < ans[i][j]) {
+                        ans[i][j] = ans[i][k]+ans[k][j];
+          		        path[i][j] = path[k][j];
+                    }
+    	return ans;
+    }
+    public static void copy(int[][] a, int[][] b) 
+    {
+        for (int i=0;i<a.length;i++)
+            for (int j=0;j<a[0].length;j++)
+                a[i][j] = b[i][j];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Tests out algorithm with graph shown in class.
+    	int[][] m = new int[5][5];
+    	m[0][0] = 0; 
+        m[0][1] = 4; 
+        m[0][2] = 8; 
+        m[0][3] = 10000; 
+        m[0][4] = 10000;
+        m[1][0] = 4; 
+        m[1][1] = 0; 
+        m[1][2] = 1; 
+        m[1][3] = 2;
+        m[1][4] = 10000;
+        m[2][0] = 8; 
+        m[2][1] = 1; 
+        m[2][2] = 0; 
+        m[2][3] = 4; 
+        m[2][4] = 2;
+        m[3][0] = 10000; 
+        m[3][1] = 2; 
+        m[3][2] = 4; 
+        m[3][3] = 0; 
+        m[3][4] = 7;
+        m[4][0] = 10000; 
+        m[4][1] = 10000; 
+        m[4][2] = 2; 
+        m[4][3] = 7;
+        m[4][4] = 0;
+        
+        int[][] path = new int[5][5];
+
+        for (int i=0; i<5; i++)
+            for (int j=0; j<5; j++)
+                if (m[i][j] == 10000)
+                    path[i][j] = -1;
+    		else
+                    path[i][j] = i;
+
+    	shortestpath(m, path);
+ 
+    	System.out.println("---------------------------");
+    	System.out.println("|  MELHOR ROTA  EQUIPE M3 |");
+    	System.out.println("---------------------------");
+        System.out.println(" ");
+        System.out.print(" Digite o ponto de partida: ");
+    	int start = sc.nextInt();
+        System.out.print(" Digite o destino: ");
+    	int end = sc.nextInt();
+         String myPath = end + "";
+         System.out.println();
+        
+    	while (path[start][end] != start) {
+            myPath = path[start][end] + " -> " + myPath;
+            end = path[start][end];
+    	}
+    	myPath = start + " -> " + myPath;
+    	System.out.println("Esta � a melhor rota: " + myPath);
+    	sc.close();
+    }
+}
+
+//La�o para imprimir os v�rtices na tela
+/*for (int i=0; i<5; i++)
+    path[i][i] = i;
+for (int i=0; i<5; i++) {
+ System.out.print("INT" + i + " ");
+} */
+
+//La�o para imprimir na tela as rotas em matriz
+/*System.out.println("  0 1 2 3 4");
+        System.out.println("  ---------");
+         for (int i=0; i<5;i++) {
+            System.out.print(i + "|");
+            for (int j=0; j<5;j++)
+                System.out.print(path[i][j]+" ");
+            System.out.println();
+    	} */
+
---
+++ src/main/java/com/nextm3project/models/TruckModel.java
@@ -0,0 +1,77 @@
+package com.nextm3project.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity	//Tornando a classe uma entidade
+@Table(name = "TRUCK")	//Criando a tabela e definindo o nome
+public class TruckModel implements Serializable {  //Serializable + serialVersionUID serve para fazer o controle das versões no JVM
+	private static final long serialVersionUID = 1L;  // ID da versão serial padrão para TruckModel
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) //Gerando o Id de forma automatica
+	private Integer id; //UUID tipo do identificador apropriado univesal, para melhor distribuicao
+	
+	@Column(nullable = false, unique = true, length = 7)
+	private String licensePlateTruck; //Placa do caminhao
+	
+	@Column(nullable = false, length = 5)
+	private String status; //Status do caminhao
+	
+	@Column(nullable = false)
+	private LocalDateTime registrationDate;  //Data de registro
+	
+	@Column(nullable = false, length = 10)
+	private String location; //localizacao (vértice)
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLicensePlateTruck() {
+		return licensePlateTruck;
+	}
+
+	public void setLicensePlateTruck(String licensePlateTruck) {
+		this.licensePlateTruck = licensePlateTruck;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(LocalDateTime registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}	
+}
