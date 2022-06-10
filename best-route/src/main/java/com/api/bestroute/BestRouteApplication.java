75b73d978f21d4e74739f981255dd88171fb4740
Diego Lins <diegotec.lins@gmail.com>
2022-06-10 09:40:26 -0300

    projeto spring boot com arquivo properties configurado

---
+++ best-route/src/main/java/com/api/bestroute/BestRouteApplication.java
@@ -0,0 +1,13 @@
+package com.api.bestroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestRouteApplication.class, args);
+	}
+
+}
---
+++ best-route/src/test/java/com/api/bestroute/BestRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.bestroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
