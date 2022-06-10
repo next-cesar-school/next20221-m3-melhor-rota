58d997e03c5e9e658b41ae9d3b2b46d6a2697c23
Dênnis Dantas <ddspog@gmail.com>
2022-06-10 10:36:58 -0300

    Spring Boot no Remoto (#2)
    * projeto spring boot com arquivo properties configurado
    
    * projeto sprin boot alterado
    
    Co-authored-by: Diego Lins <diegotec.lins@gmail.com>

---
+++ src/main/java/com/nextm3project/NextM3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nextm3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextM3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextM3ProjectApplication.class, args);
+	}
+
+}
